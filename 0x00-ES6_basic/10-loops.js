58eb052c790c5928d6c23ea467e6fc01c6c3dfc6
FrancisOkolo <francischinedu78@gmail.com>
2023-06-29 22:40:00 +0000

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of Object.keys(array)) {
+  for (let idx of Object.keys(array)) {
     const value = array[idx];
     array[idx] = appendString + value;
   }
