91a7e254c840ea2551e39cfebfbbac78bbd54294
FrancisOkolo <francischinedu78@gmail.com>
2023-08-05 10:25:17 +0000

    using Array filter() method

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 export default function getStudentsByLocation(students, city) {
-  if (students instanceOf Array) {
+  if (students instanceof Array) {
     return students.filter((student) => student.location === city);
   }
   return [];
